f2215256dac15d4dfdde9bf37607ed425e7b261e
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-10-29 09:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const apiKey = "b55e2180d1903b459d72a0fa636c2003";
+
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const main = document.getElementsByClassName("main")[0];
+const getData = async() => {
+    const data = await fetch(apiurl + "london" + `&appid=${apiKey}`);
+    const json = await data.json();
+    console.log(json.weather[0].main)
+
+    const div = document.createElement('div');
+    div.textContent = "Current weather in London: " + `${json.weather[0].main}`
+    main.append(div)
+
+}
